bd51483ed5b1371cd3b459972b252d31722f2f95
Tahi-nur <tahinurhossain@gmail.com>
2025-05-26 00:58:10 +0200

    Submit Week 2: ex5-shoppingCartPure

--- 1-JavaScript/Week2/assignment/ex5-shoppingCartPure.js
+++ 1-JavaScript/Week2/assignment/ex5-shoppingCartPure.js
@@ -1,22 +1,13 @@
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/tree/main/1-JavaScript/Week3#exercise-5-improved-shopping-at-the-supermarket
-
-In the current exercise we will rewrite the `addToShoppingCart` function to make 
-it pure. Do the following:
-
-1. Complete the parameter list of `addToShopping()`. As a first parameter it 
-   should accept a shopping cart array and as a second parameter it should 
-   accept a grocery item to be added.
-2. The function should return a new shopping cart array, following the same rule 
-   as in the previous exercise: it should contain a maximum of three items.
-3. The shopping cart passed as an argument should not be mutated.
-4. When constructing the new shopping cart array you should make use of the ES6 
-   spread syntax.
-5. Confirm that you function passes the provided unit tests.
-------------------------------------------------------------------------------*/
 // ! Function under test
-function addToShoppingCart(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+export function addToShoppingCart(cart, item) {
+  const newCart = [...cart, item]; // Add the new item using spread syntax
+
+  // If more than 3 items, remove the first
+  if (newCart.length > 3) {
+    return newCart.slice(1); // Return array without the first item
+  }
+
+  return newCart; // Otherwise, return the full cart
 }
 
 // ! Test functions (plain vanilla JavaScript)
@@ -27,8 +18,6 @@ function test1() {
 
 function test2() {
   console.log('Test 2: addToShoppingCart should be a pure function');
-  // A pure function should return the same result when called with
-  // identical arguments. It should also have no side effects (not tested here).
   const initialCart = ['bananas', 'milk'];
   const result1 = addToShoppingCart(initialCart, 'chocolate');
   const result2 = addToShoppingCart(initialCart, 'chocolate');

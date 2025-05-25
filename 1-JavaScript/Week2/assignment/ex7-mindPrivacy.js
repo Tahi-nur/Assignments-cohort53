0b8870ff7f14080c663721c78dd7ef90e1201c89
Tahi-nur <tahinurhossain@gmail.com>
2025-05-26 01:04:41 +0200

    Submit Week 2: ex7-mindPrivacy

--- 1-JavaScript/Week2/assignment/ex7-mindPrivacy.js
+++ 1-JavaScript/Week2/assignment/ex7-mindPrivacy.js
@@ -1,16 +1,3 @@
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/tree/main/1-JavaScript/Week3#exercise-7-mind-the-privacy
-
-1. Complete the `filterPrivateData()` function. It should take a single 
-   parameter: the array of employee records.
-2. It should create a _new_ array, containing employee data without the private
-   data.
-3. Use object destructuring to extract the non-private properties from an 
-   employee record (an `object`) and object literal shorthand to create a new 
-   employee record with just the non-private parts (name, occupation and email).
-4. Return the new array as the return value of the function.
-5. Run the exercise and verify that it passes all the unit tests.
-------------------------------------------------------------------------------*/
 const employeeRecords = [
   {
     name: 'John',
@@ -28,15 +15,19 @@ const employeeRecords = [
   },
 ];
 
-// ! Function under test
-function filterPrivateData(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+// ✅ Function under test
+export function filterPrivateData(employees) {
+  return employees.map(({ name, occupation, email }) => ({
+    name,
+    occupation,
+    email,
+  }));
 }
 
-// ! Test functions (plain vanilla JavaScript)
+// ✅ Test functions
 function test1() {
   console.log('Test 1: filterPrivateData should take one parameter');
-  console.assert(filterPrivateData.length === 1);
+  console.assert(filterPrivateData.length === 1, '❌ Failed Test 1');
 }
 
 function test2() {
@@ -54,7 +45,10 @@ function test2() {
     },
   ];
   const result = filterPrivateData(employeeRecords);
-  console.assert(JSON.stringify(result) === JSON.stringify(expected));
+  console.assert(
+    JSON.stringify(result) === JSON.stringify(expected),
+    '❌ Failed Test 2'
+  );
 }
 
 function test() {
